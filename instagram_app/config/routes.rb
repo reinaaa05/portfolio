68f6df0b1313898f961f999622aa5a935a90581d
reinaaa <Reinaaa0555@gmail.com>
2020-12-02 14:09:47 +0900

    user_showの追加

--- instagram_app/config/routes.rb
+++ instagram_app/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   root 'welcome#index'
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
