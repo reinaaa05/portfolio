295d7ebb6e5e304ab126326db695c33e88b5c5b5
reinaaa <Reinaaa0555@gmail.com>
2020-12-11 16:23:01 +0900

    レイアウトの変更

--- instagram_app/app/controllers/application_controller.rb
+++ instagram_app/app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:profile])
     devise_parameter_sanitizer.permit(:account_update, keys: [:profile])
   end
+
+  def after_sign_in_path_for(resource)
+    articles_path(resource)
+  end
 end
--- instagram_app/app/controllers/users_controller.rb
+++ instagram_app/app/controllers/users_controller.rb
@@ -20,7 +20,4 @@ class UsersController < ApplicationController
     @article = Article.all
   end
   
-  def after_sign_in_path_for
-    articles_path
-end
 end
