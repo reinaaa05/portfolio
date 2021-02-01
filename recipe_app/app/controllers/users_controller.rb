38a4db4499562cc1b17ef6b9fe03287382871289
reinaaa <Reinaaa0555@gmail.com>
2021-02-01 23:27:15 +0900

    スマホサイズレイアウト変更

--- recipe_app/app/controllers/users_controller.rb
+++ recipe_app/app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @posts = @user.posts.all.order(created_at: :desc)
+    @post = @user.posts.all.order(created_at: :desc)
   end
 
   def likes
