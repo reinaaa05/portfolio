71f2f5596d9879f6b1817387db7bfc3912fc2950
reinaaa <Reinaaa0555@gmail.com>
2021-02-15 19:52:59 +0900

    text_filde_tag追加

--- RailsApp/app/controllers/hello_controller.rb
+++ RailsApp/app/controllers/hello_controller.rb
@@ -1,4 +1,18 @@
 class HelloController < ApplicationController
+  protect_from_forgery
   def index
+    if request.post? then
+      @title = 'Result'
+      @msg = 'you typed: ' + params['input1'] + '.'
+      @value = params['input1']
+    else
+      @title = 'Index'
+      @msg = 'type text...'
+      @value = ''
+    end
+  end
+
+  def other
+    redirect_to action: :index, params: {'msg': 'from other page'}
   end
 end
