57f166e5e658ca8f884de13ada17510157a14880
reinaaa <Reinaaa0555@gmail.com>
2021-02-15 20:07:44 +0900

    radio_button_tag追加

--- RailsApp/app/controllers/hello_controller.rb
+++ RailsApp/app/controllers/hello_controller.rb
@@ -3,10 +3,10 @@ class HelloController < ApplicationController
   def index
     if request.post? then
       @title = 'Result'
-      if params['check1'] then
-        @msg = 'you Checked!!'
+      if params['r1'] then
+        @msg = 'you selected: ' + params['r1']
       else
-        @msg = 'not checked'
+        @msg = 'not selected...'
       end
     else
       @title = 'Index'
