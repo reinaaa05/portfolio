4bc9cf2526329c57f36c307993c71b936693cced
reinaaa <Reinaaa0555@gmail.com>
2021-02-16 23:29:18 +0900

    people_controller追加

--- RailsApp/config/routes.rb
+++ RailsApp/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'people/index'
+  get 'people', to: 'people#index'
+  get 'people/:id', to: 'people#show'
   get 'msgboard/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'hello/index'
