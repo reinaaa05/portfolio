8cbbb34dfb1f1f0c39a3461de9b139e172fea325
reinaaa <Reinaaa0555@gmail.com>
2021-02-16 16:26:16 +0900

    msgboard作成

--- RailsApp/config/routes.rb
+++ RailsApp/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'msgboard/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'hello/index'
   get 'hello', to: 'hello#index'
   get 'hello/other'
   post 'hello', to: 'hello#index'
   post 'hello/index'
+  get 'msgboard/index'
+  get 'msgboard', to: 'msgboard#index'
+  post 'msgboard', to: 'msgboard#index'
+  post 'msgboard/index'
 end
