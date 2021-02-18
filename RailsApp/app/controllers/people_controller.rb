b57fb5c6314e6325e1dc27c596019a058fdb8d37
reinaaa <Reinaaa0555@gmail.com>
2021-02-18 23:46:49 +0900

    複数検索(id)

--- RailsApp/app/controllers/people_controller.rb
+++ RailsApp/app/controllers/people_controller.rb
@@ -43,8 +43,8 @@ class PeopleController < ApplicationController
     @msg = 'please type search word...'
     @people = Array.new 
     if request.post?
-      f = '%' + params[:find] + '%'
-      @people = Person.where "name like ? or mail like ?", f,f
+      f = params[:find].split(',')
+      @people = Person.find(f)
     end
   end
 
