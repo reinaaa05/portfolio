bf815d3d35445ed99d58fbc1ec819e0f2df18127
reinaaa <Reinaaa0555@gmail.com>
2021-03-12 23:34:40 +0900

    python ポートフォリオ作成

---
+++ myapp1/bbs/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'bbs'
+
+urlpatterns = [
+  path('', views.index, name='index'),
+  path('<int:id>', views.detail, name='detail'),
+  path('create', views.create, name='create'),
+  path('<int:id>/delete', views.delete, name='delete'),
+  # path('<int:id>/update', views.update, name='update'),
+]
---
+++ myapp1/bbs/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Article
+
+def index(request):
+  articles = Article.objects.all()
+  context = {
+    'message':'Welcome my BBB',
+    'articles': articles,
+    }
+  return render(request,'bbs/index.html',context)
+
+def detail(request, id):
+  article = get_object_or_404(Article, pk=id)
+  context = {
+    'message':'Show Article' + str(id),
+    'article': article,
+    }
+  return render(request,'bbs/detail.html',context)
+
+def create(request):
+  article = Article(content='Hello BBS', user_name='paiza')
+  article.save()
+
+  articles = Article.objects.all()
+  context = {
+    'message':'Create article',
+    'articles': articles,
+    }
+  return render(request,'bbs/index.html',context)
+
+def delete(request, id):
+  article = get_object_or_404(Article, pk=id)
+  article.delete()
+
+  articles = Article.objects.all()
+  context = {
+    'message':'Delete Article' + str(id),
+    'articles': articles,
+    }
+  return render(request,'bbs/index.html',context)
