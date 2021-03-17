3276432a50459fb50b21d8ae1442781099864eb8
reinaaa <Reinaaa0555@gmail.com>
2021-03-17 23:30:18 +0900

    diaries.app追加

---
+++ helloworld.com/diaries/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+
+class Diary(models.Model):
+    category = models.CharField('カテゴリ', max_length=225)
+    text = models.TextField('本文')
+    created_at = models.DateTimeField('作成日', default=timezone.now)
+
+    def __str__(self):
+      return 'カテゴリ:{} 本文:{}' .format(self.category,self.text[:10])
---
+++ helloworld.com/myprofile/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+def top(request):
+    context = {
+        'name': 'たろう',
+    }
+    return render(request, 'myprofile/top.html', context)
+
+def resume(request):
+    return render(request, 'myprofile/resume.html')
