c7f31dd0a619e17934c93834b0490986491df8d0
reinaaa <Reinaaa0555@gmail.com>
2021-03-18 15:16:31 +0900

    Category.models追加

--- helloworld.com/diaries/models.py
+++ helloworld.com/diaries/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 from django.utils import timezone
 
+class CreatedAt(models.Model):
+    name = models.CharField('作成日',max_length=255)
+
+class Category(models.Model):
+    name = models.CharField('カテゴリ名', max_length=255)
+    created_at = models.ForeignKey(CreatedAt, verbose_name='作成日', null=True, blank=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+      return self.name
+
 class Diary(models.Model):
-    category = models.CharField('カテゴリ', max_length=225)
+    category = models.ForeignKey(Category,verbose_name='カテゴリ', on_delete=models.PROTECT)
     text = models.TextField('本文')
     created_at = models.DateTimeField('作成日', default=timezone.now)
 
     def __str__(self):
-      return 'カテゴリ:{} 本文:{}' .format(self.category,self.text[:10])
+        local_created_at = timezone.localtime(self.created_at)
+        formatted_local_created_at = local_created_at.strftime('%Y/%m/%d %H:%M:%S')
+        return 'カテゴリ:{} 本文:{}' .format(self.category,self.text[:10])
+
+    def get_title(self):
+        return 'カテゴリ:{} 本文:{}' .format(self.category, self.text[:10])
