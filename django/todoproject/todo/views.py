506956693fdbcc744c6a6e399ffc9ac587409dbb
reinaaa <Reinaaa0555@gmail.com>
2021-04-12 23:40:13 +0900

    create,update,delete 追加

--- django/todoproject/todo/views.py
+++ django/todoproject/todo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,DeleteView,UpdateView
 from .models import TodoModel
-# Create your views here.
+from django.urls import reverse_lazy
 
 class TodoList(ListView):
     template_name = 'list.html'
@@ -10,3 +10,20 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
     template_name = 'detail.html'
     model = TodoModel
+
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = TodoModel
+    fields = ('title', 'memo', 'priority', 'duedate')
+    success_url = reverse_lazy('list')
+
+class TodoDelete(DeleteView):
+    template_name = 'delete.html'
+    model = TodoModel
+    success_url = reverse_lazy('list')
+
+class TodoUpdate(UpdateView):
+    template_name = 'update.html'
+    model = TodoModel
+    fields = ('title', 'memo', 'priority', 'duedate')
+    success_url = reverse_lazy('list')
