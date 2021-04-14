c4d62f1a7b8f365da24e7d1ee019a10a25a2675b
reinaaa <Reinaaa0555@gmail.com>
2021-04-14 23:13:35 +0900

    signup追加

--- django/boardproject/boardapp/views.py
+++ django/boardproject/boardapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 def signupfunc(request):
-    object = User.objects.get(username='user01')
-    print(object.email)
     if request.method == "POST":
-        print('this is post method')
-    else:
-        print('this is not method')
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'signup.html', {'some':100})
+        except IntegrityError:
+            return render(request, 'signup.html', {'error':'このユーザーは既に登録されています登録されています'})
     return render(request, 'signup.html', {'some':100})
