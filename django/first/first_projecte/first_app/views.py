32850d6a2f41db823e93cf68c4034a5f77519992
reinaaa <Reinaaa0555@gmail.com>
2021-05-12 16:26:58 +0900

    djangoの基礎

---
+++ django/first/first_projecte/first_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('<h1>Hello</h1>')
+
+def user_page(request,user_name):
+    return HttpResponse(f'<h1>{user_name}\'s page</h1>')
+
+def number_page(request,user_name,number):
+    return HttpResponse(f'<h1>{user_name}\'s page page number = {number}</h1>')
