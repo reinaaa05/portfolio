6cbde710fc772232f2433c90f47741d1e2d06b7a
reinaaa <Reinaaa0555@gmail.com>
2021-05-17 20:43:09 +0900

    ModelProject作成

--- django/Udemy/ModelSample/ModelProject/ModelApp/admin.py
+++ django/Udemy/ModelSample/ModelProject/ModelApp/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, Prefectures, Students
 
 admin.site.register(Person)
+admin.site.register(Students)
+admin.site.register(Prefectures)
+
+
--- django/Udemy/ModelSample/ModelProject/ModelApp/models.py
+++ django/Udemy/ModelSample/ModelProject/ModelApp/models.py
@@ -26,3 +26,67 @@ class Person(BaseMeta):
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
     
+class Students(models.Model):
+
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    major = models.CharField(max_length=20)
+    school = models.ForeignKey(
+        'Schools',on_delete=models.RESTRICT
+    )
+    prefecture = models.ForeignKey(
+        'Prefectures',on_delete=models.CASCADE
+    )
+
+    class Meta:
+        db_table = 'students'
+
+class Schools(models.Model):
+    name = models.CharField(max_length=20)
+    prefecture = models.ForeignKey(
+        'Prefectures',on_delete=models.CASCADE
+    )
+    class Meta:
+        db_table = 'schools'
+
+class Prefectures(models.Model):
+    name = models.CharField(max_length=20)
+    class Meta:
+        db_table = 'prefectures'
+
+class Places(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    class Meta:
+        db_table = 'places'
+
+class Restaurants(models.Model):
+    place = models.OneToOneField(
+        Places,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'restaurants'
+
+class Authors(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'authors'
+
+class Books(models.Model):
+    name = models.CharField(max_length=50)
+    authors = models.ManyToManyField(Authors)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'books'
