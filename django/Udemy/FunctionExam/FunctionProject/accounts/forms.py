92f73005f4496e91647b550fa19b8c613aa176aa
reinaaa <Reinaaa0555@gmail.com>
2021-05-24 21:00:34 +0900

    user_edit作成

--- django/Udemy/FunctionExam/FunctionProject/accounts/forms.py
+++ django/Udemy/FunctionExam/FunctionProject/accounts/forms.py
@@ -32,3 +32,35 @@ class RegistForm(forms.ModelForm):
 class LoginForm(forms.Form):
     email = forms.CharField(label='メールアドレス')
     password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
+
+class UserEditFrom(forms.ModelForm):
+    username = forms.CharField(label='名前')
+    age = forms.IntegerField(label='年齢', min_value=0)
+    email = forms.EmailField(label='メールアドレス')
+    picture = forms.FileField(label='写真', required=False)
+
+    class Meta:
+        model = Users
+        fields = ('username', 'age', 'email', 'picture')
+
+class PasswordChangeForm(forms.ModelForm):
+    password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
+    confirm_password = forms.CharField(label='パスワード再入力', widget=forms.PasswordInput())
+    
+    class Meta():
+        model = Users
+        fields = ('password',)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data['password']
+        confirm_password = cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError('パスワードが違います')
+
+    def save(self, commit=False):
+        user = super().save(commit=False)
+        validate_password(self.cleaned_data['password'], user)
+        user.set_password(self.cleaned_data['password'])
+        user.save()
+        return user
