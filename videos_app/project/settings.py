e93189e3aa027e57bac490d8874519dd7f717620
reinaaa <Reinaaa0555@gmail.com>
2021-06-12 22:46:44 +0900

    settings.py

--- videos_app/project/settings.py
+++ videos_app/project/settings.py
@@ -76,19 +76,19 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
-    #  'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'videos_app_db',
-    #     'USER': 'reina01',
-    #     'PASSWORD': 'kumachan01' ,
-    #     'HOST': 'localhost',
-    #     'PORT': '',
+    # 'default': {
+    #      'ENGINE': 'django.db.backends.sqlite3',
+    #      'NAME': BASE_DIR / 'db.sqlite3',
     # }
+
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'videos_app_db',
+        'USER': 'reina01',
+        'PASSWORD': 'kumachan01' ,
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
 
 
